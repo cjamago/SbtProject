d3fc4211b7d7e8506772991893c37cc71b8a8742
Chijioke Amago <C.Amago@kainos.com>
2016-02-04 11:18:17 +0000

    bash file updated

--- run-functional-tests.sh
+++ run-functional-tests.sh
@@ -4,7 +4,7 @@ case $1 in
 
             "firefox"|"ie"|"chrome"|"safari"|"iphone"|"ipad"|"android"|"phantomjs"|"htmlunit")
 #                java -Xms512M -Xmx1536M -Xss1M -XX:+CMSClassUnloadingEnabled -XX:MaxPermSize=384M -Dbrowser="$1" -jar `dirname $0`/sbt-launch.jar "test-only com.bility.test.scala.Chrome"
-                 sbt -Dbrowser="$1" test
+                 sbt clean compile test -Dbrowser="$1"
                 ;;
              *) echo 'Missing browser. Usage: ./run-functional-tests.sh [firefox|ie|chrome|safari|iphone|ipad|android|phantomjs]'
                 exit -1
